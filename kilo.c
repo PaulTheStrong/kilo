bd32e0f600cd81f4c3b19525e9c422c4b837ad14
PaulTheStrong <98765432123456789r@gmail.com>
2019-12-13 21:06:26 +0300

    Step 171

--- kilo.c
+++ kilo.c
@@ -1,4 +1,4 @@
-//step 165
+//step 171
 
 /*** includes ***/
 
@@ -28,6 +28,7 @@
 #define KILO_QUIT_TIMES 3
 
 #define HL_HIGHLIGHT_NUMBERS (1<<0)
+#define HL_HIGHLIGHT_STRINGS (1<<1)
 
 enum editorKey {
 	BACKSPACE = 127,
@@ -44,6 +45,7 @@ enum editorKey {
 
 enum editorHighlight{
 	HL_NORMAL = 0,
+	HL_COMMENT,
 	HL_STRING,
 	HL_NUMBER,
 	HL_MATCH
@@ -54,6 +56,7 @@ enum editorHighlight{
 struct editorSyntax {
 	char *filetype;
 	char **filematch;
+	char *singleline_comment_start;
 	int flags;
 };
 
@@ -92,7 +95,8 @@ struct editorSyntax HLDB[] = {
 	{
 		"c",
 		C_HL_extensions,
-		HL_HIGHLIGHT_NUMBERS
+		"//",
+		HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS
 	},
 };
 
@@ -229,13 +233,47 @@ void editorUpdateSyntax(erow *row){
 	memset(row->hl, HL_NORMAL, row->rsize);
 
 	if (E.syntax == NULL) return;
+	
+	char *scs = E.syntax->singleline_comment_start;
+	int scs_len = scs ? strlen(scs) : 0;
 
 	int prev_sep = 1;
+	int in_string = 0;
 
 	int i = 0;
 	while(i < row->rsize){
 		char c = row->render[i];
 		unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
+
+		if(scs_len && !in_string){
+			if(!strncmp(&row->render[i], scs, scs_len)){
+				memset(&row->hl[i], HL_COMMENT, row->rsize - i);
+				break;
+			}
+		}
+
+		if(E.syntax->flags & HL_HIGHLIGHT_STRINGS) {
+			if(in_string) {
+				row->hl[i] = HL_STRING;
+				if(c == '\\' && i + 1 < row->rsize) {
+				row->hl[i + 1] = HL_STRING;
+				i += 2;
+				continue;
+				}
+				if (c == in_string) in_string = 0;
+				i++;
+				prev_sep = 1;
+				continue;		
+			} else {
+				if(c == '"' || c =='\'') {
+					in_string = c;
+					row->hl[i] = HL_STRING;
+					i++;
+					continue;
+				}
+			}
+		}
+
 		if(E.syntax->flags & HL_HIGHLIGHT_NUMBERS){
 			if((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) || 
 				(c == '.' && prev_hl == HL_NUMBER)){
@@ -245,7 +283,6 @@ void editorUpdateSyntax(erow *row){
 				continue;
 			}
 		}
-
 		prev_sep = is_separator(c);
 		i++;
 	}
@@ -253,6 +290,7 @@ void editorUpdateSyntax(erow *row){
 
 int editorSyntaxToColor(int hl){
 	switch (hl){
+		case HL_COMMENT : return 36;
 		case HL_STRING: return 35;
 		case HL_NUMBER: return 31;
 		case HL_MATCH: return 34;
